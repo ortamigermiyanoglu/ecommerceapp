0edafb815c25741b5fe6eb613868ca2557035a08
sumut <fatih.ek96@hotmail.com>
2021-02-21 18:15:01 +0300

    initial commit haydi bismillah

---
+++ src/main/java/com/sumutella/dolapcodecase/DolapcodecaseApplication.java
@@ -0,0 +1,13 @@
+package com.sumutella.dolapcodecase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DolapcodecaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DolapcodecaseApplication.class, args);
+    }
+
+}
